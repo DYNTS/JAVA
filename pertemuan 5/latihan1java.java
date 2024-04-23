ddc532cca7ccf878d7e694eadedf3a8f89b9ba34
DYNTS <dianyunita57931@gmail.com>
2024-04-23 20:25:37 +0700

    praktikum 6

---
+++ pertemuan 5/latihan1java.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan1;
+
+/**
+ *
+ * @author DIAN
+ */
+import javax.swing.*;
+
+public class latihan1java {
+   public static void main(String[] args){//agar dapat di run sebagai class utama
+       JFrame myWindows;
+       
+       myWindows = new JFrame(); //pembuatan object
+       myWindows.setSize(300,200);//seting ukuran form
+       myWindows.setTitle("my first java program"); //judul untuk form 
+       myWindows.setVisible(true); //untuk menampilkan form   
+   } 
+}
